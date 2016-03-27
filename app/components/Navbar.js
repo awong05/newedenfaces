ae741993c6f18f893b91e46602fc1a561ef0ae2f
Andy Wong <awong@propelmarketing.com>
2016-03-27 01:40:37 -0400

    Introducing new Navbar component, part I.

---
+++ app/components/Navbar.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import {Link} from 'react-router';
+import NavbarStore from '../stores/NavbarStore';
+import NavbarActions from '../actions/NavbarActions';
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = NavbarStore.getState();
+    this.onChange = this.onChange.bind(this);
+  }
+
+  componentDidMount() {
+  	NavbarStore.listen(this.onChange);
+  	NavbarActions.getCharacterCount();
+
+    let socket = io.connect();
+
+    socket.on('onlineUsers', (data) => {
+      NavbarActions.updateOnlineUsers(data);
+    });
+
+    $(document).ajaxStart(() => {
+      NavbarActions.updateAjaxAnimation('fadeIn');
+    });
+
+    $(document).ajaxComplete(() => {
+      setTimeout(() => {
+        NavbarActions.updateAjaxAnimation('fadeOut');
+      }, 750);
+    });
+  }
+
+  componentWillUnmount() {
+    NavbarStore.unlisten(this.onChange);
+  }
+
+  onChange(state) {
+  	this.setState(state);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    let searchQuery = this.state.searchQuery.trim();
+
+    if (searchQuery) {
+      NavbarActions.findCharacter({
+        searchQuery: searchQuery,
+        searchForm: this.refs.searchForm,
+        history: this.props.history
+      });
+    }
+  }
+}
+
+export default Navbar;
